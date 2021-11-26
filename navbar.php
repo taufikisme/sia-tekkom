f6689bf56f5cc5b81479d406ae6f349dbbae2d0d
Taufikisme <mohtaufikafandi@gmail.com>
2021-11-26 19:06:29 +0700

    feat: create navbar component

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav>
+	<a href="/">Home</a> | 
+	<a href="/mahasiswa">Mahasiswa</a> | 
+	<a href="/mata_kuliah">Mata Kuliah</a> | 
+	<a href="/dosen">Dosen</a> | 
+	<a href="/rencana_studi">Rencana Studi</a> | 
+	<a href="/logout.php">Logout</a> | 
+	Hallo <?= $_SESSION["nama"] . "!" ?>
+</nav>
