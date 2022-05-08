69c8f53da69d037c0927a7ae2037375922e1ed48
Taufikisme <mohtaufikafandi@gmail.com>
2022-05-08 11:56:12 +0700

    testing

--- admin.php
+++ admin.php
@@ -23,7 +23,7 @@
 				$row = mysqli_fetch_array($result);
 				if (password_verify($password, $row['password'])){
 					$_SESSION['nama'] = $row['nama'];
-					echo 'Berhasil Login!';
+					echo 'Anda Berhasil Login!';
 					header("Location:/");
 				} else {
 					echo 'Password salah!';
